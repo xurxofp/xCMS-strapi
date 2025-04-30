5a0df50421f4953c8885a55162de4d01e845969f
Ubuntu <ubuntu@ip-172-26-0-43.eu-west-3.compute.internal>
2025-04-30 23:06:39 +0000

    changed middleware to ts

---
+++ config/middlewares.ts
@@ -0,0 +1,32 @@
+type EnvFn = (key: string, defaultValue?: any) => string | number | boolean;
+
+console.log('[🛡️ CSP] cargando config/middlewares.ts personalizada');
+
+
+export default ({ env }: { env: EnvFn }) => [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src":  ["'self'", "https:"],
+          "img-src":      ["'self'", "data:", "blob:", env("CDN_URL")],
+          "media-src":    ["'self'", "data:", "blob:", env("CDN_URL")],
+          // anula la directiva upgradeInsecureRequests (opcional)
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::public',
+];
